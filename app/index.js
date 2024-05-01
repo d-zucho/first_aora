beed492399501e9f0b6e77d5b3e57b2e8f95835a
D_Zucho <danny.nico.dev@outlook.com>
2024-05-01 07:21:49 -0700

    add initial bottom tab bar

--- app/index.js
+++ app/index.js
@@ -5,10 +5,10 @@ import { Link } from 'expo-router'
 export default function App() {
   return (
     <View className='flex-1 items-center justify-center bg-white'>
-      <Text className='text-3xl'>Hello!</Text>
+      <Text className='text-3xl font-pblack'>Aora</Text>
       <StatusBar style='auto' />
-      <Link href='/profile' className='text-blue-500'>
-        Profile
+      <Link href='/home' className='text-blue-500'>
+        Go to Home
       </Link>
     </View>
   )
